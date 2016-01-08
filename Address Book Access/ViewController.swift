13cec7fc195b00ff6cd27672225788ea1acf7b7f
Philippe petit <ppet@mac-server.home>
2016-01-08 17:39:01 +0100

    Initial Commit

---
+++ Address Book Access/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Address Book Access
+//
+//  Created by Philippe petit on 08/01/2016.
+//  Copyright © 2016 Philippe petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Address Book AccessTests/Address_Book_AccessTests.swift
@@ -0,0 +1,36 @@
+//
+//  Address_Book_AccessTests.swift
+//  Address Book AccessTests
+//
+//  Created by Philippe petit on 08/01/2016.
+//  Copyright © 2016 Philippe petit. All rights reserved.
+//
+
+import XCTest
+@testable import Address_Book_Access
+
+class Address_Book_AccessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

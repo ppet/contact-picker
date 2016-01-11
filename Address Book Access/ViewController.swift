8954e5a8a6bbaa6c1b2908a44fff6fd81614c57c
Philippe petit <ppet@mac-server.home>
2016-01-11 18:07:28 +0100

    completed version

--- Address Book Access/ViewController.swift
+++ Address Book Access/ViewController.swift
@@ -5,21 +5,71 @@
 //  Created by Philippe petit on 08/01/2016.
 //  Copyright © 2016 Philippe petit. All rights reserved.
 //
+//  Voir: stackoverflow.com/questions/2521183999 tout en bas
+//
 
 import UIKit
+import ContactsUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CNContactPickerDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var firstName: UILabel!
+    @IBOutlet weak var phoneNumber: UILabel!
+    
+    let peoplePicker = CNContactPickerViewController()
+    //var addressBook: ABAddressBookRef?
+
+    // MARK: Actions
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        peoplePicker.delegate = self
+        //people.displayedProperties = [NSNumber(int: kABPersonEmailProperty)]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func showPicker(sender: UIButton) {
+        print("In showPicker")
+        //let picker = ABPeoplePickerNavigationController()
+        //picker.peoplePickerDelegate = self
+        self.presentViewController(peoplePicker, animated: true, completion: nil)
+    }
+    
+    func contactPickerDidCancel(picker: CNContactPickerViewController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func contactPicker(picker: CNContactPickerViewController, didSelectContact person: CNContact) {
+        print("In didSelectContact")
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+        let phone = person.phoneNumbers[0].value as? CNPhoneNumber
+        print("countryCode = \(phone!.valueForKey("countryCode")!)")
+        print("digits = \(phone!.valueForKey("digits")!)")
+        phoneNumber.text = phone!.stringValue
+        
+        let personName = person.givenName + " " + person.familyName
+        print("personName = \(personName)")
+        firstName.text = personName
+        
+        
+    }
+    
+    func contactPicker(peoplePicker: CNContactPickerViewController, shouldContinueAfterSelectingPerson person: CNContact, property: CNContactProperty) -> Bool {
+        print("In shouldContinueAfterSelectingPerson")
+        
+        contactPicker(peoplePicker, didSelectContact: person)
+        
+        peoplePicker.dismissViewControllerAnimated(true, completion: nil)
+        
+        return false
+    }
+    
 
 }
 
